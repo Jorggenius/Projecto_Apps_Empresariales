c1a340639db96e3c41a77002a7348bb1eb800867
jorge luis <martinezmonajorgeluis@gmail.com>
2025-08-30 19:38:34 -0500

    Se inicializa el proyecto con un a base de springBoot

---
+++ noche-app/noche-app/src/main/java/com/schoolDays/noche_app/NocheAppApplication.java
@@ -0,0 +1,13 @@
+package com.schoolDays.noche_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocheAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NocheAppApplication.class, args);
+	}
+
+}
---
+++ noche-app/noche-app/src/test/java/com/schoolDays/noche_app/NocheAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolDays.noche_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NocheAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
