216ec51d7c6d42b62471698a7e5cf43a7daa7808
jorge luis <martinezmonajorgeluis@gmail.com>
2025-09-11 19:41:28 -0500

    completo repositories

--- noche-app/noche-app/src/main/java/com/schoolDays/noche_app/persistenceLayer/repository/InscripciónRepository.java
+++ noche-app/noche-app/src/main/java/com/schoolDays/noche_app/persistenceLayer/repository/InscripciónRepository.java
@@ -1,4 +1,9 @@
 package com.schoolDays.noche_app.persistenceLayer.repository;
 
-public interface InscripciónRepository {
+import com.schoolDays.noche_app.persistenceLayer.entity.InscripcionEntity;
+import com.schoolDays.noche_app.persistenceLayer.entity.ModuloEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface InscripciónRepository extends CrudRepository<InscripcionEntity, Long> {
+
 }
