a44ce7a9317d4392fe03c3f7ccc9a12b6e9b29d2
jorge luis <martinezmonajorgeluis@gmail.com>
2025-09-13 17:29:56 -0500

    este es un error en nuestro trabajo

--- noche-app/noche-app/src/main/java/com/schoolDays/noche_app/persistenceLayer/repository/MóduloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.schoolDays.noche_app.persistenceLayer.repository;
-
-import com.schoolDays.noche_app.persistenceLayer.entity.ModuloEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MóduloRepository extends CrudRepository<ModuloEntity, Long> {
-}
--- noche-app/src/main/java/com/schoolDays/noche_app/persistenceLayer/repository/InscripciónRepository.java
+++
@@ -1,9 +0,0 @@
-package com.schoolDays.noche_app.persistenceLayer.repository;
-
-import com.schoolDays.noche_app.persistenceLayer.entity.InscripcionEntity;
-import com.schoolDays.noche_app.persistenceLayer.entity.ModuloEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface InscripciónRepository extends CrudRepository<InscripcionEntity, Long> {
-
-}
